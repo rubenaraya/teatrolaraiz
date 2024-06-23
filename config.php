8373b15ce36621e216ee6bb7a8ac12e76bb50f80
Ruben Araya Tagle <rubenarayatagle@gmail.com>
2024-06-22 20:12:19 -0400

    atualizacion

---
+++ config.php
@@ -0,0 +1,15 @@
+<?php
+$titulo = 'Teatro La Raíz';
+$descripcion = 'Teatro de arte dramático';
+$keywords = 'Teatro, Miniaturas, Pedagogía, Identidad, Comunidad';
+$url = 'https://www.teatrolaraiz.cl/web/';
+$ga = 'G-CZ6G1N3EBC';
+$video = 'https://www.youtube.com/embed/videoseries?si=we4GbApZw28NNazU&list=PLWshe2IHPsMUoJ8SwLhoudf97XClkK86N';
+$instagram = 'https://www.instagram.com/teatrolaraiz/';
+$youtube = 'http://www.youtube.com/@Teatrolaraiz';
+$facebook = 'https://web.facebook.com/Teatrolaraiz/';
+$linkedin = 'https://www.linkedin.com/in/teatro-la-raíz-192052274/';
+$twitter = 'https://x.com/teatro_la_raiz';
+$maps = 'https://maps.app.goo.gl/E53u8Mnu9tjQrGoz8';
+$mapa = 'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3329.262785797283!2d-70.6711882!3d-33.4424595!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x9662c4536b4b783b%3A0x8966fbac9033bf2e!2sGarc%C3%ADa%20Reyes%20243%2C%208350502%20Santiago%2C%20Regi%C3%B3n%20Metropolitana!5e0!3m2!1ses-419!2scl!4v1718993566149!5m2!1ses-419!2scl';
+?>
---
+++ js/base.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var nombre = document.getElementById('nombre');
+    var user = nombre.textContent;
+    var domain = 'teatrolaraiz.cl';
+    nombre.innerHTML = '<a href="mailto:' + user + '@' + domain + '" class="text-dark">' + user + '@' + domain + '</a>';
+    new WOW().init();
+});
