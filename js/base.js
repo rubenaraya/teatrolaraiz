668fda59c34ae949ec5d69843e07b25a71bb8fec
Ruben Araya Tagle <rubenarayatagle@gmail.com>
2024-06-23 14:11:23 -0400

    actualizacion

--- js/base.js
+++ js/base.js
@@ -2,6 +2,6 @@ document.addEventListener('DOMContentLoaded', function() {
     var nombre = document.getElementById('nombre');
     var user = nombre.textContent;
     var domain = 'teatrolaraiz.cl';
-    nombre.innerHTML = '<a href="mailto:' + user + '@' + domain + '" class="text-dark">' + user + '@' + domain + '</a>';
+    nombre.innerHTML = '<a href="mailto:' + user + '@' + domain + '" class="text-body">' + user + '@' + domain + '</a>';
     new WOW().init();
 });
