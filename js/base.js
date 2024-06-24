1cae97a7169e2e6996b304a55751af5939aa7d7a
Ruben Araya Tagle <rubenarayatagle@gmail.com>
2024-06-24 14:57:55 -0400

    actualizacion

--- js/base.js
+++ js/base.js
@@ -1,6 +1,10 @@
 document.addEventListener('DOMContentLoaded', function() {
-    var nombre = document.getElementById('nombre');
+    var nombre = document.getElementById('contacto');
     var user = nombre.textContent;
     nombre.innerHTML = '<a href="mailto:' + user + '@' + dominio + '">' + user + '@' + dominio + '</a>';
     new WOW().init();
+    const spinner = document.querySelector('#spinner');
+    if (spinner) {
+        spinner.style.display = 'none';
+    }
 });
