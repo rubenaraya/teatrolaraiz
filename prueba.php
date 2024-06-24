5536061543c48c7bd5996ce0a72de624be2b7bc5
Ruben Araya Tagle <rubenarayatagle@gmail.com>
2024-06-24 17:06:26 -0400

    actualizacion

---
+++ prueba.php
@@ -0,0 +1,11 @@
+<?php
+include('config.php');
+$archivo = __DIR__ . '/html/prueba.html';
+
+no_cache();
+if (file_exists($archivo)) {
+    $texto = file_get_contents($archivo);
+    $texto = incluir_archivos($texto);
+    echo reemplazar_textos($texto);
+}
+?>
