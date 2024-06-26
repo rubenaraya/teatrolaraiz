a38c68545d0fe363bbb9281f45be03cb71bacfbe
Ruben Araya Tagle <rubenarayatagle@gmail.com>
2024-06-26 12:39:32 -0400

    actualizacion

--- js/base.js
+++ js/base.js
@@ -1,9 +1,7 @@
 //js\base.js
+
 class Web {
-    constructor(dominio, cookie) {
-        this.ELEMENTOS = new Map();
-		this.dominio = dominio;
-		this.cookie = cookie;
+    constructor() {
     }
 	_ocultarSpinner() {
 		const spinner = document.querySelector('#mi_spinner');
@@ -39,36 +37,10 @@ class Web {
 		}));
 	}
 	_activarTooltips() {
+		const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
+		const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
 	}
-	_activarToast() {
-		const notificacion = document.querySelector('#mi_notificacion');
-		//mi_notificacion
-	}
-	/* Adaptado de: Bootstrap Cookie Alert by Wruczek
-	* https://github.com/Wruczek/Bootstrap-Cookie-Alert
-	* Released under MIT license */
-	_escribirCookie(cname, cvalue, exdays) {
-		let d = new Date();
-		d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
-		let expires = "expires=" + d.toUTCString();
-		document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
-	}
-	_leerCookie(cname) {
-		let name = cname + "=";
-		let decodedCookie = decodeURIComponent(document.cookie);
-		let ca = decodedCookie.split(';');
-		for (var i = 0; i < ca.length; i++) {
-			var c = ca[i];
-			while (c.charAt(0) === ' ') {
-				c = c.substring(1);
-			}
-			if (c.indexOf(name) === 0) {
-				return c.substring(name.length, c.length);
-			}
-		}
-		return "";
-	}
-	_comprobarCookie() {
+	_comprobarCookie(nombre) {
 		"use strict";
 		let cookieAlert = document.querySelector(".mi_alerta_cookies");
 		let acceptCookies = document.querySelector(".mi_aceptar_cookies");
@@ -76,43 +48,84 @@ class Web {
 			return;
 		}
 		cookieAlert.offsetHeight;
-		if (this._leerCookie(this.cookie).length == 0) {
+		if (leerCookie(nombre).length == 0) {
 			cookieAlert.classList.add("show");
 		}
 		acceptCookies.addEventListener("click", function () {
-			this._escribirCookie(this.cookie, "0", 365);
+			escribirCookie(nombre, "0", 365);
 			cookieAlert.classList.remove("show");
 			window.dispatchEvent(new Event("cookieAlertAccept"))
 		});
 	}
-	_mostrarContacto() {
+	_mostrarContacto(dominio) {
 		let nombre = document.querySelector('#contacto');
 		let user = nombre.textContent;
-		nombre.innerHTML = '<a href="mailto:' + user + '@' + this.dominio + '">' + user + '@' + this.dominio + '</a>';
+		nombre.innerHTML = '<a href="mailto:' + user + '@' + dominio + '">' + user + '@' + dominio + '</a>';
 	}
-	_mostrarNotificacion(mensaje, tipo) {
-		
-	}
-	
+
 	volverArriba() {
 		window.scrollTo({ top: 0, behavior: 'smooth' });
 	}
+	mostrarNotificacion(texto, tipo) {
+		const notificacion = document.querySelector('#mi_notificacion');
+		const mensaje = document.querySelector('#mi_mensaje');
+		if (notificacion && mensaje) {
+			notificacion.classList.remove('text-bg-success');
+			notificacion.classList.remove('text-bg-danger');
+			if (tipo=='EXITO') {
+				notificacion.classList.add('text-bg-success');
+			} else {
+				notificacion.classList.add('text-bg-danger');
+			}
+			mensaje.innerHTML = texto;
+			const toast = bootstrap.Toast.getOrCreateInstance(notificacion)
+			toast.show();
+		}
+	}
 	enviarMensaje() {
+		//TODO: Falta que valide el formulario y lo envíe
 		const modal = bootstrap.Modal.getInstance('#formContacto');
 		modal.hide();
-	
+		//TODO: Falta seleccionar el tipo de notificación según el resultado
+		let exito = 'Tu mensaje fue enviado exitosamente, pronto nos contactaremos contigo';
+		let error = 'No fue posible enviar tu mensaje, intenta nuevamente en otro momento';
+		this.mostrarNotificacion(exito, 'EXITO');
 	}
-			
 }
-const W = new Web(dominio, cookie);
+
+/* Adaptado de: Bootstrap Cookie Alert by Wruczek
+* https://github.com/Wruczek/Bootstrap-Cookie-Alert
+* Released under MIT license */
+function escribirCookie(cname, cvalue, exdays) {
+	let d = new Date();
+	d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
+	let expires = "expires=" + d.toUTCString();
+	document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
+function leerCookie(cname) {
+	let name = cname + "=";
+	let decodedCookie = decodeURIComponent(document.cookie);
+	let ca = decodedCookie.split(';');
+	for (var i = 0; i < ca.length; i++) {
+		var c = ca[i];
+		while (c.charAt(0) === ' ') {
+			c = c.substring(1);
+		}
+		if (c.indexOf(name) === 0) {
+			return c.substring(name.length, c.length);
+		}
+	}
+	return "";
+}
+
+const W = new Web();
 
 document.addEventListener('DOMContentLoaded', function() {
     new WOW().init();
-	W._mostrarContacto();
-	W._ocultarSpinner();
+    W._comprobarCookie(cookie);
+	W._mostrarContacto(dominio);
 	W._activarGaleria('galeria');
-	W._activarToast();
+	W._ocultarSpinner();
 	W._activarTooltips();
 	W._mostrarScrollTop();
-    W._comprobarCookie(cookie);
 });
