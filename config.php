1fc7afdf9a17d9ff8e52fc0a1bf1132423b6220e
Ruben Araya Tagle <rubenarayatagle@gmail.com>
2024-06-29 11:27:24 -0400

    actualizacion

--- config.php
+++ config.php
@@ -15,41 +15,41 @@ $textos = array(
     'twitter'=>'https://x.com/teatro_la_raiz',
     'maps'=>'https://maps.app.goo.gl/E53u8Mnu9tjQrGoz8',
     'mapa'=>'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3329.262785797283!2d-70.6711882!3d-33.4424595!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x9662c4536b4b783b%3A0x8966fbac9033bf2e!2sGarc%C3%ADa%20Reyes%20243%2C%208350502%20Santiago%2C%20Regi%C3%B3n%20Metropolitana!5e0!3m2!1ses-419!2scl!4v1718993566149!5m2!1ses-419!2scl',
-    'inicio'=>'prueba.php',
+    'inicio'=>'/web',
 );
 
 $archivos = array(
-    'bootstrap'=>'_bootstrap.html',
-    'ga'=>'_ga.html',
-    'iconos'=>'_iconos.html',
-    'simbolos'=>'_simbolos.html',
-    'nocache'=>'_nocache.html',
-    'metadatos'=>'_metadatos.html',
-    'area_auxiliar'=>'area_auxiliar.html',
-    'area_tarjetas_ico'=>'area_tarjetas_ico.html',
-    'area_tarjetas_img'=>'area_tarjetas_img.html',
-    'area_paralaje'=>'area_paralaje.html',
-    'area_video'=>'area_video.html',
-    'area_mapa'=>'area_mapa.html',
-    'area_personas'=>'area_personas.html',
-    'area_testimonios'=>'area_testimonios.html',
-    'area_etiquetas'=>'area_etiquetas.html',
-    'area_galeria'=>'area_galeria.html',
-    'area_carrusel'=>'area_carrusel.html',
-    'area_acordeon'=>'area_acordeon.html',
-    'area_contactar'=>'area_contactar.html',
-    'area_menu_h'=>'area_menu_h.html',
-    'area_menu_v'=>'area_menu_v.html',
-    'area_productos'=>'area_productos.html',
-    'area_panel_video'=>'area_panel_video.html',
-    'area_panel_diapo'=>'area_panel_diapo.html',
-    'area_calendario'=>'area_calendario.html',
-    'area_botonera'=>'area_botonera.html',
-    'area_encabezado'=>'area_encabezado.html',
-    'area_pie'=>'area_pie.html',
-    'area_temporal'=>'area_temporal.html',
-    'area_cargador'=>'area_cargador.html',
-    'area_fondo_video'=>'area_fondo_video.html',
+    '_bootstrap'=>'/html/bloques/_bootstrap.html',
+    '_ga'=>'/html/bloques/_ga.html',
+    '_iconos'=>'/html/bloques/_iconos.html',
+    '_simbolos'=>'/html/bloques/_simbolos.html',
+    '_nocache'=>'/html/bloques/_nocache.html',
+    '_metadatos'=>'/html/bloques/_metadatos.html',
+    'auxiliar'=>'/html/areas/auxiliar.html',
+    'tarjetas_ico'=>'/html/areas/tarjetas_ico.html',
+    'tarjetas_img'=>'/html/areas/tarjetas_img.html',
+    'paralaje'=>'/html/areas/paralaje.html',
+    'video'=>'/html/areas/video.html',
+    'mapa'=>'/html/areas/mapa.html',
+    'personas'=>'/html/areas/personas.html',
+    'testimonios'=>'/html/areas/testimonios.html',
+    'etiquetas'=>'/html/areas/etiquetas.html',
+    'galeria'=>'/html/areas/galeria.html',
+    'carrusel'=>'/html/areas/carrusel.html',
+    'acordeon'=>'/html/areas/acordeon.html',
+    'form_contacto'=>'/html/areas/form_contacto.html',
+    'menu_h'=>'/html/areas/menu_h.html',
+    'menu_v'=>'/html/areas/menu_v.html',
+    'productos'=>'/html/areas/productos.html',
+    'panel_video'=>'/html/areas/panel_video.html',
+    'panel_diapo'=>'/html/areas/panel_diapo.html',
+    'calendario'=>'/html/areas/calendario.html',
+    'botonera'=>'/html/areas/botonera.html',
+    'encabezado'=>'/html/areas/encabezado.html',
+    'pie'=>'/html/areas/pie.html',
+    'temporal'=>'/html/areas/temporal.html',
+    'cargador'=>'/html/areas/cargador.html',
+    'fondo_video'=>'/html/areas/fondo_video.html',
 );
 
 function reemplazar_textos($texto) {
@@ -65,7 +65,7 @@ function reemplazar_textos($texto) {
 function incluir_archivos($texto) {
     global $archivos;
     foreach ( $archivos as $clave => $valor ) {
-        $ruta_archivo = __DIR__ . '/html//'. $valor;
+        $ruta_archivo = __DIR__ . $valor;
         if (file_exists($ruta_archivo)) {
             $agregar = file_get_contents($ruta_archivo);
             $texto = str_replace( '(('.$clave.'))', $agregar, $texto );
--- prueba.php
+++ prueba.php
@@ -1,7 +1,12 @@
 <?php
 include('config.php');
-$archivo = __DIR__ . '/html/prueba.html';
 
+if (isset($_GET['p'])) {$p=$_GET['p'];} else {$p='1';}
+$patron = __DIR__ . '/html/prueba-{p}.html';
+$archivo = str_replace('{p}', $p, $patron);
+if (file_exists($archivo)==false) {
+    $archivo = str_replace('{p}', '1', $patron);
+}
 no_cache();
 if (file_exists($archivo)) {
     $texto = file_get_contents($archivo);
